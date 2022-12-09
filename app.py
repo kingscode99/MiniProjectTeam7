37c51a375973b8387feaca52c71d738d5efaac0d
kingscode99 <118798407+kingscode99@users.noreply.github.com>
2022-12-09 20:38:57 +0900

    update

--- app.py
+++ app.py
@@ -16,17 +16,10 @@ def home():
 def main_post():
     title_receive = request.form['title_give']
     comment_receive = request.form['comment_give']
-    print(title_receive, comment_receive)
-
     doc = {'title': title_receive, 'comment': comment_receive}
-    db.mini.insert_one(doc)
+    db.projects.insert_one(doc)
     return jsonify({'msg': '등록 완료!'})
 
-
-# @app.route("/first/main/post", methods=["GET"])
-# def main_get():
-#     post_list = list(db.login.find({}, {'_id': False}))
-#     return jsonify({'msg': '등록 완료',post_list:'샘플샘플'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
