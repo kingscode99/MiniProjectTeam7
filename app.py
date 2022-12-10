439482514aa4a2b1d6c7efacd838a69afdf22596
kingscode99 <118798407+kingscode99@users.noreply.github.com>
2022-12-10 15:36:02 +0900

    main board 구현

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 
-//각자 파이몽고 url사용해 보세요!!
 from pymongo import MongoClient
 client = MongoClient('mongodb+srv://test:sparta@cluster0.b6vbteu.mongodb.net/cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
@@ -11,6 +10,13 @@ db = client.dbsparta
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/first/main/profile', methods=['GET'])
+def test_get():
+    user_list = list(db.users.find({}, {'_id': False}))
+    post_list = list(db.projects.find({}, {'_id': False}))
+    return jsonify({'users': user_list, 'posts': post_list})
 
 
 @app.route("/first/main/post", methods=["POST"])
