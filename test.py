a9d80df25decbd1a14f357cca6b5ba5c8798c2b8
kingscode99 <118798407+kingscode99@users.noreply.github.com>
2022-12-11 22:29:23 +0900

    merge feaure/23_LeeSungjin

--- test.py
+++ test.py
@@ -3,9 +3,10 @@ app = Flask(__name__)
 
 import certifi
 
+# 각자 파이몽고 계정으로 테스트해주세요~
 from pymongo import MongoClient
 ca=certifi.where()
-client = MongoClient("mongodb+srv://아이디:패스워드@Cluster1.vpb9hys.mongodb.net/?retryWrites=true&w=majority", tlsCAFile=ca)
+client = MongoClient("mongodb+srv://lcoeda:test@Cluster1.vpb9hys.mongodb.net/?retryWrites=true&w=majority", tlsCAFile=ca)
 db = client.test
 
 SECRET_KEY = 'PROJECT_7'
@@ -18,27 +19,21 @@ import hashlib
 
 @app.route('/')
 def home():
+    return render_template('home.html')
+
+@app.route('/login')
+def login():
+
     return render_template('login.html')
 
-    token_receive= request.cookies.get('token')
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        user_info = db.min7_project.find_one({"id": payload['id']})
-        return render_template('sign_up.html', nickname=user_info["nick"])
-    except jwt.exceptions.DecodeError:
-        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
-    # 타이머 로그 아웃은 필요할지 안할지 모르겠어서 구현 안했음
-
-@app.route('/api', methods=['POST'])
+@app.route('/api/login', methods=['POST'])
 def api_login():
     id_receive = request.form['id_give']
     pw_receive = request.form['pw_give']
 
-    # 회원가입 때와 같은 방법으로 pw를 암호화합니다.
     pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
 
-    # id, 암호화된pw을 가지고 해당 유저를 찾습니다.
-    result = db.min7_pj.find_one({'id': id_receive, 'pw': pw_hash})
+    result = db.min7_project.find_one({'id': id_receive, 'pw': pw_hash})
 
     # 찾으면 JWT 토큰을 만들어 발급합니다.
     if result is not None:
@@ -58,16 +53,23 @@ def api_login():
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
-@app.route('/sign', methods=["POST"])
-def sign_up():
+
+@app.route('/sign', methods=["GET"])
+def show_sign():
+    return render_template('sign_up.html')
+
+@app.route('/api/sign', methods=["post"])
+def def_sign():
     id_receive = request.form['id_give']
     pw_receive = request.form['pw_give']
     name_receive = request.form['name_give']
     nick_receive = request.form['nick_give']
 
-    doc = {       # 패스워드에 경우 해쉬값으로 저장해야하지만 일단은 뼈대 구축만 하겠다.
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+
+    doc = {  # 패스워드에 경우 해쉬값으로 저장해야하지만 일단은 뼈대 구축만 하겠다.
         'id': id_receive,
-        'pw': pw_receive,
+        'pw': pw_hash,
         'name': name_receive,
         'nick': nick_receive
     }
@@ -75,10 +77,6 @@ def sign_up():
     db.min7_project.insert_one(doc)
 
     return jsonify({'msg': '변경 완료!'})
-
-@app.route('/sign', methods=["GET"])
-def show_sign():
-    return render_template('sign_up.html')
 
 @app.route('/main',methods=["GET"])
 def show_main(): # 로그인 성공시 main으로 가는지 확인하기 위함
